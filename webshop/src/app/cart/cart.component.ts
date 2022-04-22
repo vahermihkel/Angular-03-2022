e6baed92f6113927cf156ac7e82223b0c125dfc7
vahermihkel <vahermihkel@gmail.com>
2022-04-22 11:14:41 +0300

    ebayst andmed, cart-s kogused

--- webshop/src/app/cart/cart.component.ts
+++ webshop/src/app/cart/cart.component.ts
@@ -6,10 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  cartProducts: any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    const cartItemsSS = sessionStorage.getItem("cartItems");
+    if (cartItemsSS) {
+      this.cartProducts = JSON.parse(cartItemsSS); 
+    }
   }
+
+  onDecreaseQuantity(cartProduct: any) {
+    cartProduct.quantity--;
+    if (cartProduct.quantity <= 0) {
+      this.onRemoveProduct(cartProduct);
+    }
+    sessionStorage.setItem("cartItems", JSON.stringify(this.cartProducts));
+  }
+
+  onIncreaseQuantity(cartProduct: any) {
+    cartProduct.quantity++;
+    sessionStorage.setItem("cartItems", JSON.stringify(this.cartProducts));
+  }
+
+  onRemoveProduct(cartProduct: any) {
+              // this.cartProducts.indexOf(cartProduct);
+    const index = this.cartProducts.findIndex(element => element.product.id === cartProduct.product.id);
+    if (index >= 0) {
+      this.cartProducts.splice(index,1);
+      sessionStorage.setItem("cartItems", JSON.stringify(this.cartProducts));
+    }
+  }
+
+  // ISESEISVALT: ostukorvi kogusumma
+  // kui võtate hinna, siis peab olema element.product.price
+  // korrutada läbi ka kogusega
+
+  // MAKSMINE
+
+  // TÜHJENDA NUPP
+
+  // *ngIf'ga ära kustutamine kui on 0 toodet 
+  // MAKSMISE NUPP ja TÜHJENDA NUPP
 
 }
--- webshop/src/app/home/home.component.ts
+++ webshop/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  products: any[] = [];
+  dbUrl = "https://webshop-03-22-default-rtdb.europe-west1.firebasedatabase.app/products.json";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>(this.dbUrl).subscribe(response => {   // .subscribe lubab edasi minna
+        //  {-asdasd: {1}, -aqeqe, {2}}      [{1},{2}]   ---> forin tsükkel   (teeb objekti sees tsükli)
+                    // const toode = {nimi: "Coca cola", hind: 3, kategooria: "coca", aktiivne: true}
+                    // const newArray = [];
+                    // for (const key in toode)    1. nimi   2. hind    3. kategooria    4. aktiivne
+                    //   toode[key]    1. "Coca cola"   2. 3   3. "coca"   4. true
+                    // forin sees:    newArray.push(toode[key])     ->    ["Coca cola", 3, "coca", true];
+        // const newArray = [];
+        for (const key in response) {
+          this.products.push(response[key]);
+        }
+        // this.products = newArray;
+    }); 
+  }
+
+  // [{1},{2},{3},{1},{1},{2},{1}]
+  // [{toode: {1}, kogus: 4},{toode: {2}, kogus: 2}, {toode: {3}, kogus: 1}]
+                //{id: 3, nimi: "C", hind: 32}
+  onAddToCart(productClicked: any) {
+    const cartItemsSS = sessionStorage.getItem("cartItems");
+    let cartItems: any[] = [];
+    if (cartItemsSS) { // cartItemsSS !== null
+      cartItems = JSON.parse(cartItemsSS);
+    }   // [{id: 1},{id: 2}, {id: 3}]
+                                //                                            3
+    const index = cartItems.findIndex(element => element.product.id === productClicked.id);
+    if (index >= 0) {
+      cartItems[index].quantity++;  // suurenda täpselt 1 võrra
+      // cartItems[index].quantity += 2;
+      // cartItems[index].quantity = cartItems[index].quantity/2 + 2;
+    } else {
+      cartItems.push({ product: productClicked, quantity: 1 });
+    }
+
+    // enne kui pushin otsi üles kas sellist toodet juba on ostukorvi esemete hulgas
+    // sulgude seest tuleva eseme ID ---> product.id
+    // otsin kas seda on cartItems seas  ----> .findIndex(element => element.id === product.id)
+    // kui ON, siis teen ühte loogikat
+    // kui EI OLE, siis teen teist loogikat
+    // if ()  index >= 0  ---> suurendan kogust
+    // else  index === -1  --->   lisan ostukorvi  .push abil 
+
+    sessionStorage.setItem("cartItems", JSON.stringify(cartItems));
   }
 
 }
